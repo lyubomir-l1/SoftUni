7906e5edf8f1242e2c4e8e172c8b759102fa1c64
Lyubomir Lyubenov <lyubomir2017@gmail.com>
2022-09-25 20:48:26 +0300

    Delete function onTimeForTheExam(input){.js

--- basic sintax- exercise/function onTimeForTheExam(input){.js
+++
@@ -1,5 +0,0 @@
-function integerAndFloat(first, second, last){
-let sum = first + second + last;
-sum % 1 == 0 ? console.log(`${sum} - Integer`) : console.log(`${sum} - Float`)
-}
-integerAndFloat( 9, 100, 1.1)
